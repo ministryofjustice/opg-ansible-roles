22dedd8794ca63c15b68c7580e970fbc789b6991
tim-mcarthur-moj <tim.mcarthur@digital.justice.gov.uk>
2017-02-13 11:22:57 +0000

    add changes for efs targets re-order launch config cleanup tasks

--- filter_plugins/opg-custom.py
+++ filter_plugins/opg-custom.py
@@ -9,18 +9,19 @@ def dict_to_list(dict_to_convert):
 
 
 def generate_identifier(stackname, slice_length=10):
-    '''
+    """
     :param stackname:
     :param cutoff_length:
     :param slice_length:
     :return:
-    '''
+    """
     import hashlib
 
     stackname = hashlib.sha1(stackname).hexdigest()
     stackname = stackname[0:slice_length]
 
     return stackname
+
 
 def unique_instance_stacks(instance_list, target):
     stack_names = []
@@ -34,8 +35,29 @@ def unique_instance_stacks(instance_list, target):
 
     return list(set(stack_names))
 
+
 def split_part(string, index, separator='-'):
     return string.split(separator)[index]
+
+
+def make_efs_targets(source_list, extra_item):
+    """
+    create list suitable for use with EFS module target argument
+    :param source_list:
+    :param extra_item:
+    :return:
+    """
+    if not isinstance(source_list, list):
+        raise errors.AnsibleFilterError('First argument must be of type list')
+    if not isinstance(extra_item, basestring):
+        raise errors.AnsibleFilterError('Second argument must be of type string')
+    if len(source_list) < 1 or len(extra_item) < 1:
+        raise errors.AnsibleFilterError('Values passed cannot be zero length')
+    new_list = []
+    for list_item in source_list:
+        new_list.append({'subnet_id': list_item, 'security_groups': [ extra_item ]})
+    return new_list
+
 
 class FilterModule(object):
 
@@ -44,6 +66,7 @@ class FilterModule(object):
             'dict_to_list': dict_to_list,
             'generate_identifier': generate_identifier,
             'unique_instance_stacks': unique_instance_stacks,
-            'split_part': split_part
+            'split_part': split_part,
+            'make_efs_targets': make_efs_targets
         }
         return filter_list
